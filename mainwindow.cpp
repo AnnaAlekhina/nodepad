6106e6fe05b668c5b8d185288dc9d78c3c891b57
AnnaGaus <anyagayagays95@mail.ru>
2015-07-07 19:56:28 +0400

    node

---
+++ mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<iostream>
+#include<fstream>
+#include <QFileDialog>
+#include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_QuitButton_clicked()
+{
+    qApp->quit();
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(),
+               tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
+
+       if (!fileName.isEmpty()) {
+           QFile file(fileName);
+           if (!file.open(QIODevice::ReadOnly)) {
+               QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
+               return;
+           }
+           QTextStream in(&file);
+           ui->textEdit->setText(in.readAll());
+           file.close();
+       }
+}
